e09018045b75e8cfa2135b376132fa2ee845eef2
Pranil Shripad <pranilshripad35@gmail.com>
2024-06-02 23:56:15 +0530

    Remove Exercise directory

--- Exercises/Exercise1.py
+++
@@ -1,8 +0,0 @@
-name = "Pranil Shripad"
-Address = """C-408,
-NIT Complex,
-Ayurvedic Layout,
-Sakkardara,
-Nagpur - 440024"""
-print(name)
-print(Address)
--- Exercises/ReverseWordOrder.py
+++
@@ -1,5 +0,0 @@
-s = input("Enter a sentence: ")
-
-result = s.split(" ")
-fresult = ' '.join(result[::-1])   
-print(fresult)
